e1469c7b3db8e8d4f47862569ac59e6a9d3d8d70
Cláudio José Ramos <cjramos96@protonmail.com>
2025-03-18 10:26:48 -0300

    Adicionar arquivos de projeto.

---
+++ AuthJwtSolution/Ports/IAuth.cs
@@ -0,0 +1,10 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthJwtSolution.Ports
+{
+    public interface IAuth
+    {
+        public string JwtAuthHandler(string nome);
+    }
+}
---
+++ AuthJwtSolution/Services/AuthService.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using AuthJwtSolution.Ports;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthJwtSolution.Services
+{
+    public class AuthService : IAuth
+    {
+        private readonly IConfiguration _config;
+        public AuthService(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string JwtAuthHandler(string nome)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["AuthSecretKey"])), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                SigningCredentials = credentials,
+                Expires = DateTime.UtcNow.AddMinutes(5),
+                Claims = new Dictionary<string, object>
+                {
+                    { "nome", nome }
+                }
+            };
+
+            var token = handler.CreateToken(tokenDescriptor);
+
+            return handler.WriteToken(token);
+        }
+    }
+}
